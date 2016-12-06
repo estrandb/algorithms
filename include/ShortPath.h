6f9e44d8eae754b181ee4c2dc1d16f55b5b8b178
Eddie <estrandb@gmail.com>
2016-12-05 21:24:10 -0800

    starting work on Dijkstra

---
+++ include/ShortPath.h
@@ -0,0 +1,20 @@
+#ifndef SHORTPATH_H
+#define SHORTPATH_H
+
+#include <stdint.h>
+
+class ShortPath
+{
+    public:
+        ShortPath(uint32_t vertices){X = new uint32_t[vertices];}
+        virtual ~ShortPath(){delete[] X;}
+
+    protected:
+
+    private:
+    uint32_t* X;
+    uint32_t* A;
+
+};
+
+#endif // SHORTPATH_H
