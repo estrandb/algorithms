af613edece64c33a1ae51ac7fa86a8898bb5fdf3
Eddie <estrandb@gmail.com>
2017-01-10 13:11:07 -0800

    2sumdone

--- src/TwoSum.cpp
+++ src/TwoSum.cpp
@@ -16,29 +16,50 @@ void TwoSum::Run()
 {
     Utilities* util = new Utilities();
     std::unordered_map<int64_t, int64_t>* hmap = new std::unordered_map<int64_t, int64_t>();
-    std::string const filePath = "/home/sig/projects/algorithms/prob1test.txt";
+    std::unordered_map<int64_t, int64_t>* hmapUsed = new std::unordered_map<int64_t, int64_t>();
+    std::string const filePath = "/home/sig/projects/algorithms/prob1.txt";
 
     util->HashThis(*hmap, filePath);
     std::cout << hmap->size() << std::endl;
 
     int64_t counter = 0;
+    int64_t t = 0;
 
-    for (int64_t t = -10000; t <= 10000; t++)
+    for (t = -10000; t <= 10000; t++)
     {
         for (auto it = hmap->begin(); it != hmap->end(); ++it)
         {
-            auto got = hmap->find(t - it->first);
-            if (got == hmap->end())
+            int64_t x = it->second;
+            int64_t y = t-x;
+            if ((hmapUsed->find(x) != hmapUsed->end() && hmapUsed->at(x) == y) || (hmapUsed->find(y) != hmapUsed->end() && hmapUsed->at(y) == x))
+            {
                 continue;
-            int64_t y = hmap->at(t - it->first);
-            if (it->first != y)
+            }
+            auto gotY = hmap->find(y);
+            if (gotY == hmap->end())
+                continue;
+            if (x != y)
             {
                 counter++;
+                hmapUsed->insert({x,y});
             }
+            if (counter > 427)
+            {
+                std::cout << "Counter overflowed" << std::endl;
+                break;
+            }
+        }
+        hmapUsed->clear();
+        //std::cout << "t = " << t << std::endl;
+        if (counter > 427)
+        {
+            std::cout << "Counter overflowed" << std::endl;
+            break;
         }
     }
     std::cout << counter << std::endl;
 
     delete hmap;
+    //delete hmapUsed;
     delete util;
 }
