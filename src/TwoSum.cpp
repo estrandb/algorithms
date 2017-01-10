1fa7101df3f9e23f35fde687927d84cb89ee6407
Eddie <estrandb@gmail.com>
2017-01-10 10:53:00 -0800

    issues with twosum

--- src/TwoSum.cpp
+++ src/TwoSum.cpp
@@ -16,10 +16,28 @@ void TwoSum::Run()
 {
     Utilities* util = new Utilities();
     std::unordered_map<int64_t, int64_t>* hmap = new std::unordered_map<int64_t, int64_t>();
-    std::string const filePath = "/home/sig/projects/algorithms/prob1.txt";
+    std::string const filePath = "/home/sig/projects/algorithms/prob1test.txt";
 
     util->HashThis(*hmap, filePath);
     std::cout << hmap->size() << std::endl;
+
+    int64_t counter = 0;
+
+    for (int64_t t = -10000; t <= 10000; t++)
+    {
+        for (auto it = hmap->begin(); it != hmap->end(); ++it)
+        {
+            auto got = hmap->find(t - it->first);
+            if (got == hmap->end())
+                continue;
+            int64_t y = hmap->at(t - it->first);
+            if (it->first != y)
+            {
+                counter++;
+            }
+        }
+    }
+    std::cout << counter << std::endl;
 
     delete hmap;
     delete util;
